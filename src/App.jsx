61c4ab6218491db189db8833be52161cf1f8e10b
nicoisz <nicolasignacio.sz@gmail.com>
2023-09-06 16:30:37 -0300

    adding images, creating mock server

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import HotelList from "./components/HotelList";
+import { Route, Switch } from "wouter";
+
+const client = new QueryClient();
+
 function App() {
-  return <h1>hola</h1>;
+  return (
+    <QueryClientProvider client={client}>
+      <HotelList />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/components/HotelList.jsx
+++ src/components/HotelList.jsx
@@ -63,3 +63,5 @@ function HotelList() {
     </>
   );
 }
+
+export default HotelList;
